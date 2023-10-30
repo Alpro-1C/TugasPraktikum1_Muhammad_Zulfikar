484aa14745bd028d8e5152a2c3584051660ad477
Zoeeel <147987372+Zoeeel@users.noreply.github.com>
2023-10-30 22:37:28 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   cout << "--------------------------------------------------------------------------------------------\n";
+   cout << "Nama : Muhammad Zulfikar Abdi Rahman\n\n";
+   cout << "NPM : 2310631170141\n\n";
+   cout << "Kelas : 1C - Informatika\n\n";
+   cout << "Cita - Cita : SUKSES\n\n";
+   cout << "Motivasi masuk prodi : karena ilmu informatika sangat berperan penting dalam perubahan jaman\n";
+   cout << "--------------------------------------------------------------------------------------------";
+}
